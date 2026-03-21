51d97caa1a9843b9d26366ecb53a82d13a81b5d1
Juan F. Mosquera <juanfmx2@gmail.com>
2026-03-21 18:45:31 +0000

    initial version to scrape data from CVA website

--- src-util/ScrapeCVAData.ts
+++ src-util/ScrapeCVAData.ts
@@ -1,30 +1,30 @@
 import { chromium, firefox, webkit, Browser, Page } from 'playwright';
 
 interface BrowserData {
-browser: string;
-title: string;
-url: string;
+    browser: string;
+    title: string;
+    url: string;
 }
 
 async function scrapeBrowserTestPage(): Promise<BrowserData[]> {
 const browsers = [
-{ name: 'Chromium', instance: await chromium.launch() },
-{ name: 'Firefox', instance: await firefox.launch() },
-{ name: 'WebKit', instance: await webkit.launch() }
+    { name: 'Chromium', instance: await chromium.launch() },
+    { name: 'Firefox', instance: await firefox.launch() },
+    { name: 'WebKit', instance: await webkit.launch() }
 ];
 
 const results: BrowserData[] = [];
 
 for (const { name, instance } of browsers) {
-const page = await instance.newPage();
-try {
-await page.goto('https://www.whatismybrowser.com/');
-const title = await page.title();
-const url = page.url();
-results.push({ browser: name, title, url });
-} finally {
-await instance.close();
-}
+    const page = await instance.newPage();
+    try {
+        await page.goto('https://www.cambsvolleyball.org.uk/');
+        const title = await page.title();
+        const url = page.url();
+        results.push({ browser: name, title, url });
+    } finally {
+        await instance.close();
+    }
 }
 
 return results;
