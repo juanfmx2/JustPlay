71c9de0d73c1e0e12a9d3774de796ab2d19b7c18
Juan F. Mosquera <juanfmx2@gmail.com>
2026-03-28 01:53:30 +0000

    add logo on the home

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,47 +1,20 @@
-import * as fs from 'fs'
-import { createFileRoute, useRouter } from '@tanstack/react-router'
-import { createServerFn } from '@tanstack/react-start'
-
-const filePath = 'count.txt'
-
-async function readCount() {
-  return parseInt(
-    await fs.promises.readFile(filePath, 'utf-8').catch(() => '0'),
-  )
-}
-
-const getCount = createServerFn({
-  method: 'GET',
-}).handler(() => {
-  return readCount()
-})
-
-const updateCount = createServerFn({ method: 'POST' })
-  .inputValidator((d: number) => d)
-  .handler(async ({ data }) => {
-    const count = await readCount()
-    await fs.promises.writeFile(filePath, `${count + data}`)
-  })
+import { createFileRoute } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/')({
   component: Home,
-  loader: async () => await getCount(),
 })
 
 function Home() {
-  const router = useRouter()
-  const state = Route.useLoaderData()
-
   return (
-    <button
-      type="button" style={{ height: '2000px' }}
-      onClick={() => {
-        updateCount({ data: 1 }).then(() => {
-          router.invalidate()
-        })
-      }}
-    >
-      Add 1 to {state}?
-    </button>
+    <section className="container-fluid py-4">
+      <div className="d-flex flex-column align-items-center justify-content-center gap-3 text-center">
+        <img
+          src="/img/banana.png"
+          alt="Banana"
+          className="img-fluid"
+          style={{ maxWidth: '420px', width: '100%', height: 'auto' }}
+        />
+      </div>
+    </section>
   )
 }
